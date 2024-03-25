a3dca3f9c93c6694dcf7e7fa0a6f5b5b0ea4753e
RepellantMold <repellantmold@outlook.com>
2024-03-25 12:15:39 -0700

    change scripts into a single makefile

--- gba_build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set CROSSPREFIX=arm-none-eabi-
-
-%CROSSPREFIX%gcc -std=c11 -Wall -Wextra -Werror -O3 -nostdlib -march=armv4t -mtune=arm7tdmi -mthumb-interwork -T gba.ld -o player_gba.o head.S player_gba.c -lgcc
-%CROSSPREFIX%objcopy -O binary player_gba.o player_gba.bin
-%CROSSPREFIX%gcc -std=c11 -Wall -Wextra -Werror -Os -nostdlib -march=armv4t -mtune=arm7tdmi -mthumb-interwork -T boot.ld -o gba_f3m.o boot.S
-%CROSSPREFIX%objcopy -O binary gba_f3m.o gba_f3m.gba
